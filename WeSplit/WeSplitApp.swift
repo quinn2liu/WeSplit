9a8a83681f16869ebd1dab37275c38737695bd2a
Quinn Liu <quinn2liu@gmail.com>
2024-07-19 21:02:54 -0400

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Quinn Liu on 7/19/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
